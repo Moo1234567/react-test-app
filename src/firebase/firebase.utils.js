4eb5bfd2d0b9d00535fab04822998796a4d43fea
Евгений Татарчук <spik_13@mail.ru>
2020-03-17 07:04:33 +0300

    firebase auth and signup and store in firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,25 +1,51 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyC5mytj7Tb091AKeuRGx8zomb683XL9HsM",
-    authDomain: "react-test-app-55dde.firebaseapp.com",
-    databaseURL: "https://react-test-app-55dde.firebaseio.com",
-    projectId: "react-test-app-55dde",
-    storageBucket: "react-test-app-55dde.appspot.com",
-    messagingSenderId: "317942131230",
-    appId: "1:317942131230:web:9f0764bb5f6dc26bfe9c0f",
-    measurementId: "G-G3ZXNE1NJM"
-  };
+  apiKey: "AIzaSyC5mytj7Tb091AKeuRGx8zomb683XL9HsM",
+  authDomain: "react-test-app-55dde.firebaseapp.com",
+  databaseURL: "https://react-test-app-55dde.firebaseio.com",
+  projectId: "react-test-app-55dde",
+  storageBucket: "react-test-app-55dde.appspot.com",
+  messagingSenderId: "317942131230",
+  appId: "1:317942131230:web:9f0764bb5f6dc26bfe9c0f",
+  measurementId: "G-G3ZXNE1NJM"
+};
 
-  firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+  const snapShot = await userRef.get();
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt: 'select_account'});
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  if (!snapShot.exist) {
+    const {displayName, email, logoUrl} = userAuth;
+    const createdAt = new Date();
+    try{
+      const createUser = await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (err){
+      console.log('error creating user', err.message)
+    };
+  
+  }
+  console.log(snapShot);
+  return userRef;
+};
 
-  export default firebase;
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
